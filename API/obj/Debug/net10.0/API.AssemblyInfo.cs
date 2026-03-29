0384e8be85ec7ffc4936196fc5b55f97c1b945c7
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-03-29 20:17:58 +0100

    Added seed data and a way to automatically add or not migrations for anyone initially running the project

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a3c7f1aa96901a6803a0e28088f4eecdbb7b69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5668f18af803dce9d54a1cd51c9ab31a33fa1f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
