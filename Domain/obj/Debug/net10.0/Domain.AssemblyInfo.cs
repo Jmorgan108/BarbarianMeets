c7602319d1d2538553d6082228f37a231c80187f
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-05-11 23:00:34 +0100

    Update Api WIP

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5668f18af803dce9d54a1cd51c9ab31a33fa1f98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0384e8be85ec7ffc4936196fc5b55f97c1b945c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
